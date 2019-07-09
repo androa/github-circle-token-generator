3cbc74b45e2f95153ac0f71a29cef574ce302095
André Roaldseth <andre@roaldseth.net>
2019-07-09 09:53:01 +0200

    Use port from Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const jwt = require('jsonwebtoken')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const APP_ID = process.env.GITHUB_APP_ID;
 const privateKey = process.env.GITHUB_APP_KEY;
 const circleToken = process.env.CIRCLECI_TOKEN
