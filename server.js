fe9ded81539d9d7e753e7b4a2f0e9ae182c2ae60
André Roaldseth <andre@roaldseth.net>
2019-07-09 10:40:37 +0200

    Add a bit verbose logging

--- server.js
+++ server.js
@@ -1,59 +1,69 @@
-const axios = require('axios')
-const CircleCi = require('circleci')
-const express = require('express')
-const fs = require('fs')
-const jwt = require('jsonwebtoken')
+const axios = require("axios");
+const CircleCi = require("circleci");
+const express = require("express");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 const APP_ID = process.env.GITHUB_APP_ID;
 const privateKey = process.env.GITHUB_APP_KEY;
-const circleToken = process.env.CIRCLECI_TOKEN
+const circleToken = process.env.CIRCLECI_TOKEN;
 const ci = new CircleCi({
   auth: circleToken
-})
+});
 
 const token = (req, res) => {
-  var token = jwt.sign({}, privateKey,
-    {
-      algorithm: 'RS256',
-      expiresIn: '10m',
-      issuer: APP_ID
-    }
-  );
+  var token = jwt.sign({}, privateKey, {
+    algorithm: "RS256",
+    expiresIn: "10m",
+    issuer: APP_ID
+  });
 
-  return token
-}
+  return token;
+};
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/test-token', (req, res) => {
-  const tokens = token()
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/test-token", (req, res) => {
+  const tokens = token();
 
-  axios.get('https://api.github.com/app', {
-    headers: {
-      'Authorization': `Bearer ${tokens}`
-    }
-  }).then(({data}) => res.send).catch(l => { console.log(l);res.send(l)})
-})
-
-app.get('/inject-token', (req, res) => {
-  const tokens = token()
   console.log(`Made new token ${tokens}`);
 
-  const request ={
+  axios
+    .get("https://api.github.com/app", {
+      headers: {
+        Authorization: `Bearer ${tokens}`
+      }
+    })
+    .then(({ data }) => res.send)
+    .catch(l => {
+      console.log(l);
+      res.send(l);
+    });
+});
+
+app.get("/inject-token", (req, res) => {
+  const tokens = token();
+
+  console.log(`Made new token ${tokens}`);
+
+  const request = {
     username: req.query.username,
     project: req.query.project,
     body: {
-      name: 'GITHUB_TOKEN',
-      value: tokens,
+      name: "GITHUB_TOKEN",
+      value: tokens
     }
-  }
+  };
 
-  console.log(request)
-  ci.setEnvVar(request).then((f) => {
-    console.log(f)
-    res.send(f)})
-  .catch(res.send)
-})
+  console.log(`Installing token for ${username}/${project}`);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  ci.setEnvVar(request)
+    .then(f => {
+      console.log(f);
+      res.send(f);
+    })
+    .catch(res.send);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
