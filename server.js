8eb3830aaaf64a1a5344fe48990ce0205aba90c5
André Roaldseth <andre@roaldseth.net>
2019-07-15 12:52:55 +0200

    Use winston for logging

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const app = require('./lib/app');
+const express = require("express");
+const app = require("./lib/app");
+const morgan = require("morgan");
+const logger = require("./lib/logger");
+
+// Kill express.js default error logger, but keep error handling
+console.error = () => {};
 
 const port = process.env.PORT || 3000;
+const server = express()
+  .use(morgan("combined", { stream: logger.stream }))
+  .use(app)
+  .use(logger.logErrors)
+  .listen(port, () => logger.info(`Example app listening on port ${port}!`));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-process.on('SIGINT', function() {
+process.on("SIGINT", function() {
   server.close(() => {
     process.exit(0);
   });
